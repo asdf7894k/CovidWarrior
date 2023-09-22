211626f76424b7f89caaf2027b9a827134e0e9f6
Jun <asdf7894k@gmail.com>
2023-09-22 16:44:34 +0800

    ServiceWorker Test

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "Penpeer-Moderna-Covid-Vaccine-0.0.4";
-const contentToCache = [
-    "Build/CovidWarrior.loader.js?id=0.0.4",
-    "Build/CovidWarrior.framework.js.unityweb?id=0.0.4",
-    "Build/CovidWarrior.data.unityweb?id=0.0.4",
-    "Build/CovidWarrior.wasm.unityweb?id=0.0.4",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
