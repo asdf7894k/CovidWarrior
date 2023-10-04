c1377642d88a4f5d79c378e8a07dc96bec31b3ce
Jun <asdf7894k@gmail.com>
2023-10-04 15:37:50 +0800

    Remove service worker

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,49 +1,9 @@
-const cacheName = "0.1.2";
-const contentToCache = [
-    "Build/CovidWarrior.loader.js?id=0.1.2",
-    "Build/CovidWarrior.framework.js.unityweb?id=0.1.2",
-    "Build/CovidWarrior.data.unityweb?id=0.1.2",
-    "Build/CovidWarrior.wasm.unityweb?id=0.1.2",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
 
     self.skipWaiting();
  
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheName !== "0.1.2") {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
